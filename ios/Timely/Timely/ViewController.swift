dbc7977e9f0262a39521c4a9636e8792fa779829
Sean Hess <seanhess@gmail.com>
2019-02-14 12:55:00 -0700

    added ios project

---
+++ ios/Timely/Timely/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timely
+//
+//  Created by Sean Hess on 2/14/19.
+//  Copyright © 2019 Timely Advance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
